793fac5c55ffcf23c562e20236a9adb7a119c0a8
eylulbalcilar <eylulbalcilar@gmail.com>
2026-05-11 15:32:37 +0200

    add swap test

---
+++ test/PointsHook.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+ 
+import {Test} from "forge-std/Test.sol";
+ 
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+ 
+
+import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+
+ 
+import {Currency} from "v4-core/types/Currency.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
+ 
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+ 
+import {ERC1155TokenReceiver} from "solmate/src/tokens/ERC1155.sol";
+
+import {PointsHook} from "../src/PointsHook.sol";
+ 
+contract TestPointsHook is Test, Deployers, ERC1155TokenReceiver {
+ 
+	MockERC20 token;
+ 
+
+	Currency ethCurrency = Currency.wrap(address(0));
+	Currency tokenCurrency;
+ 
+	PointsHook hook;
+
+ function setUp() public {
+  
+    deployFreshManagerAndRouters();
+ 
+
+    token = new MockERC20("Test Token", "TEST", 18);
+    tokenCurrency = Currency.wrap(address(token));
+ 
+
+    token.mint(address(this), 1000 ether);
+    token.mint(address(1), 1000 ether);
+ 
+  
+    uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
+    deployCodeTo("PointsHook.sol", abi.encode(manager), address(flags));
+ 
+    hook = PointsHook(address(flags));
+ 
+    token.approve(address(swapRouter), type(uint256).max);
+    token.approve(address(modifyLiquidityRouter), type(uint256).max);
+ 
+    (key, ) = initPool(
+        ethCurrency, 
+        tokenCurrency, 
+        hook, 
+        3000, 
+        SQRT_PRICE_1_1 
+    );
+ 
+
+    uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
+ 
+    uint256 ethToAdd = 0.003 ether;
+    uint128 liquidityDelta = LiquidityAmounts.getLiquidityForAmount0(
+        SQRT_PRICE_1_1,
+        sqrtPriceAtTickUpper,
+        ethToAdd
+    );
+ 
+    modifyLiquidityRouter.modifyLiquidity{value: ethToAdd}(
+        key,
+        ModifyLiquidityParams({
+            tickLower: -60,
+            tickUpper: 60,
+            liquidityDelta: int256(uint256(liquidityDelta)),
+            salt: bytes32(0)
+        }),
+        ZERO_BYTES
+    );
+}
+
+function test_swap() public {
+    uint256 poolIdUint = uint256(PoolId.unwrap(key.toId()));
+    uint256 pointsBalanceOriginal = hook.balanceOf(
+        address(this),
+        poolIdUint
+    );
+ 
+    bytes memory hookData = abi.encode(address(this));
+ 
+    swapRouter.swap{value: 0.001 ether}(
+        key,
+        SwapParams({
+            zeroForOne: true,
+            amountSpecified: -0.001 ether, // Exact input for output swap
+            sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+        }),
+        PoolSwapTest.TestSettings({
+            takeClaims: false,
+            settleUsingBurn: false
+        }),
+        hookData
+    );
+    uint256 pointsBalanceAfterSwap = hook.balanceOf(
+        address(this),
+        poolIdUint
+    );
+    assertEq(pointsBalanceAfterSwap - pointsBalanceOriginal, 2 * 10 ** 14);
+}
+}
